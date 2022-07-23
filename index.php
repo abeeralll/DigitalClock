629187dd0cc4a1fa8d9b189ff13ac47df234d108
abeeralll <abiralshrestha337@gmail.com>
2022-07-23 11:40:44 +1000

    few changes

--- index.php
+++ index.php
@@ -41,7 +41,8 @@
                         echo ("<p align=center>London, England" . "<br>"); ?>
                         <?php date_default_timezone_set("Europe/London"); ?>
                         <?= date("h") ?><span id="separator">:</span><?= date("i") . date(" A") . "<br>" ?>
-                        <?= date("l") . ", " . date("Y-M-d");
+                        <?= date("l") . ", " . date("Y-M-d");  
+                        
                         ?>
 
                     </div>
